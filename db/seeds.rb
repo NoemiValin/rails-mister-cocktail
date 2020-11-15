c0d801ffbc216d0d38ec42dfd9297d9e151046d6
Noemi Valin <noemivalin@outlook.fr>
2020-11-15 17:36:46 +0100

    mister cocktail pas terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Dose.create(description: "4.5cl")
+Dose.create(description: "9.5cl")
+Dose.create(description: "2cl")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Margarita")
